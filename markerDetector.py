fa6cfefa054f209849cd1f3a33137f2ad9a32053
co16661666 <62578553+co16661666@users.noreply.github.com>
2026-03-27 14:12:45 -0400

    moved marker detector setup and initialization outside of main function

--- markerDetector.py
+++ markerDetector.py
@@ -4,22 +4,22 @@ import numpy as np
 # # Load the image
 # image = cv2.imread('marker_screenshot.png')
 
+aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
+    
+parameters = cv2.aruco.DetectorParameters()
+
+parameters.cornerRefinementMethod = cv2.aruco.CORNER_REFINE_SUBPIX # Also try CORNER_REFINE_CONTOUR
+# parameters.cornerRefinementMethod = cv2.aruco.CORNER_REFINE_APRILTAG
+# parameters.cornerRefinementWinSize = 2
+# parameters.cornerRefinementMinAccuracy = 0.05
+# parameters.cornerRefinementMaxIterations = 30 # default
+
+# Create the ArUco detector
+detector = cv2.aruco.ArucoDetector(aruco_dict, parameters)
+
 def getCorners(image):
     if image is None:
         return []
-    
-    aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
-    
-    parameters = cv2.aruco.DetectorParameters()
-
-    parameters.cornerRefinementMethod = cv2.aruco.CORNER_REFINE_SUBPIX # Also try CORNER_REFINE_CONTOUR
-    # parameters.cornerRefinementMethod = cv2.aruco.CORNER_REFINE_APRILTAG
-    # parameters.cornerRefinementWinSize = 2
-    # parameters.cornerRefinementMinAccuracy = 0.05
-    # parameters.cornerRefinementMaxIterations = 30 # default
-
-    # Create the ArUco detector
-    detector = cv2.aruco.ArucoDetector(aruco_dict, parameters)
 
     # Detect the markers
     corners, ids, rejected = detector.detectMarkers(image)
